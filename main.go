9d0eb2ca5b01239ff061da78a9df21b8f906734f
Sebastian Barfurth <s@barfurth.com>
2025-05-01 09:51:12 +0000

    remove react

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"getting-married/gen/guest/v1/guestv1connect"
 	"getting-married/guest"
 	"log"
@@ -11,13 +13,13 @@ import (
 	"golang.org/x/net/http2/h2c"
 )
 
-const (
-	addr   = "0.0.0.0:8080"
-	dbFile = "getting_married.db"
+var (
+	port   = flag.Int("port", 8080, "The port at the server listens.")
+	dbPath = flag.String("db_path", "getting_married.db", "Path to the BoltDB database file.")
 )
 
 func main() {
-	boltDB, err := bolt.Open(dbFile, 0600, nil)
+	boltDB, err := bolt.Open(*dbPath, 0600, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,9 +34,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle(path, handler)
 
-	fs := http.FileServer(http.Dir("./out"))
-	mux.Handle("/", fs)
-
+	addr := fmt.Sprintf(":%d", *port)
 	log.Printf("listening on %s", addr)
 	if err := http.ListenAndServe(addr, h2c.NewHandler(mux, &http2.Server{})); err != nil {
 		log.Fatalf("failed serve: %v", err)
